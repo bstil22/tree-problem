bb08204d5bb25a366995b9fed760da978ebe00da
bstil22 <bradydynamite@hotmail.com>
2015-06-10 12:32:33 -0700

    initial commit

---
+++ tree.rb
@@ -0,0 +1,29 @@
+class Tree
+    def initialize
+        @inp = $stdin.read.split("\n")
+    end
+    def utopian_tree
+        @inp.drop(1).each do |cycles|
+            final_height(cycles.to_i)
+        end
+    end
+    def final_height(cycles)
+        if cycles == 0
+            puts 1
+        else 
+            start = 0
+            height = 1
+            (1..cycles).each  do |el|
+                if el.even?
+                    height = height + 1
+                else
+                    height = height * 2
+                end
+            end
+             puts height    
+         end
+    end
+end   
+
+a = Tree.new
+a.utopian_tree
